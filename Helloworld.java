aed6000fb6259ea3e1bcfbb4ccef3d2c2669831f
Shanmukh Anaparthi <110324769+Shanmukh025@users.noreply.github.com>
2023-12-27 11:44:19 +0530

    Update Helloworld.java

--- Helloworld.java
+++ Helloworld.java
@@ -1,7 +1,5 @@
-public class Helloworld
-{
-    public static void main(String []args)
-    {
-        System.out.println("My First CICD Program");
-    }
+public class HelloWorld {
+  public static void main(String[] args) {
+    System.out.println("Hello, World");
+  }
 }
